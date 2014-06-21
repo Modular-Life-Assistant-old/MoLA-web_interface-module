48e5725648c25d0ca6cfaec29de64cc11b018d87
Treemo <treemo@hotmail.fr>
2014-06-21 12:22:52 +0200

    add home page

--- Module.py
+++ Module.py
@@ -1,11 +1,47 @@
+from core import Log
+
+import bottle
+import logging
+import os
+
 class Module:
-    def __init__(self):
-        print('init')
+    def add_home_page(self, name, handle):
+        self.__home_page_available[name] = handle
 
     def load_configuration(self):
-        print('load_configuration')
+        self.__home_page_available = {}
 
-    def thread_1(self):
-        while True:
-            print('thread 1')
-            time.sleep(1)
+        bottle.get('/')(self.__home_page)
+        bottle.post('/')(self.__home_page)
+
+        try:
+            module_path = os.path.dirname(os.path.abspath(__file__))
+            with open('%slog.conf' % module_path) as config_file:
+                self.__config = json.load(config_file)
+
+        except EnvironmentError:
+            # default config
+            self.__config = {
+                'port':         14212,
+                'home_page':    '',
+            }
+
+    def thread_run(self):
+        bottle.TEMPLATE_PATH[:0] = [
+            '%s/views' % os.path.dirname(os.path.abspath(__file__)),
+        ]
+
+        if Log.get_logger().isEnabledFor(logging.DEBUG):
+            bottle.debug()
+
+        bottle.run(host='0.0.0.0', port=self.__config['port'], quiet=True)
+
+    def __home_page(self):
+
+        if self.__config['home_page'] in self.__home_page_available:
+            return self.__home_page_available[self.__config['home_page']]
+
+        if self.__home_page_available:
+            return self.__home_page_available.itervalues().next()
+
+        return 'Hello world'
